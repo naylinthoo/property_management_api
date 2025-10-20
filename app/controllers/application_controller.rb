1283cd16e0570774930718ebb86d07daaf70a411
naylin <nayylinhtoo@gmail.com>
2025-10-20 14:29:56 +0700

    [update] CI/CD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_api_key!
+
+  private
+
+  def authenticate_api_key!
+    provided_key = request.headers['X-API-KEY']
+
+    if provided_key.blank? || provided_key != ENV['API_KEY']
+      render json: { error: 'Unauthorized: Invalid API key' }, status: :unauthorized
+    end
+  end
 end
